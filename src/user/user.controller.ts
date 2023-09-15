27e18cefd24562f0fe201cae5daace358e001428
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2023-09-15 16:05:39 -0300

    create log interceptor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,53 +1,59 @@
-import { Controller, Post, Body, Get, Param, Patch, Put, Delete, ParseIntPipe } from '@nestjs/common'
-import { CreateUserDTO } from './dto/create-user.dto'
-import { UpdatePutUserDTO } from './dto/update-put-user.dto'
-import { UpdatePatchUserDTO } from './dto/update-patch-user.dto'
-import { UserService } from './user.service'
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Put,
+  Delete,
+  ParseIntPipe,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
 
-@Controller("users")
+@Controller('users')
+// @UseInterceptors(LogInterceptor)
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) { }
+  @Post()
+  async create(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
+  }
 
-    @Post()
-    async create(@Body() data: CreateUserDTO) {
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
 
-        return this.userService.create(data)
-    }
+  @Get(':id')
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findById(id);
+  }
 
-    @Get()
-    async findAll() {
+  @Put(':id')
+  async update(
+    @Body() data: UpdatePutUserDTO,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.userService.update(id, data);
+  }
 
-        return this.userService.findAll()
-    }
+  @Patch(':id')
+  async updatePartial(
+    @Body() data: UpdatePatchUserDTO,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.userService.updatePartial(id, data);
+  }
 
-    @Get(':id')
-    async findById(@Param('id', ParseIntPipe) id: number) {
-
-        return this.userService.findById(id)
-    }
-
-    @Put(':id')
-    async update(
-        @Body() data: UpdatePutUserDTO,
-        @Param('id', ParseIntPipe) id: number
-    ) {
-
-        return this.userService.update(id, data)
-    }
-
-    @Patch(':id')
-    async updatePartial(
-        @Body() data: UpdatePatchUserDTO,
-        @Param('id', ParseIntPipe) id: number
-    ) {
-
-        return this.userService.updatePartial(id, data)
-    }
-
-    @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id: number) {
-
-        return this.userService.delete(id)
-    }
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
+  }
 }
